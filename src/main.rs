8db39690d7ed6ea6282d2eb5e7d3ce55b327d9eb
golmschenk <greg@olmschenk.com>
2023-07-28 00:14:44 -0400

    Add function for installing nu if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use std::error::Error;
-use std::io;
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let command_to_check = "nu"; // Replace this with the command you want to check
-    if command_exists(command_to_check) {
-        println!("The command '{}' exists.", command_to_check);
-    } else {
-        println!("The command '{}' does not exist.", command_to_check);
-    };
+    handle_nu();
 
     Ok(())
+}
+
+fn handle_nu() {
+    let nu_command_string = "nu";
+    if command_exists(nu_command_string) {
+        println!("The command '{}' exists.", nu_command_string);
+    } else {
+        println!("The command '{}' does not exist.", nu_command_string);
+        install_nu();
+    };
+}
+
+fn install_nu() {
+    let mut nu_install_command = Command::new("cargo");
+    nu_install_command.arg("install");
+    nu_install_command.arg("nu");
+    nu_install_command.arg("--features=dataframe");
+    match nu_install_command.output()
+    {
+        Ok(output) => match output.status.success() {
+            true => (),
+            false => panic!("Exit status of `{:?}` was unexpectedly false.", nu_install_command)
+        },
+        Err(error) => panic!("Error running `{:?}`: `{:?}`.", nu_install_command, error),
+    }
 }
 
 fn command_exists(cmd: &str) -> bool {
