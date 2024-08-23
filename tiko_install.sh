1cddaba64372d69135b4838a727bde7363d54f8a
golmschenk <greg@olmschenk.com>
2024-08-23 18:02:26 -0400

    Correct conda initialization

--- tiko_install.sh
+++ tiko_install.sh
@@ -7,22 +7,27 @@ echo "Logging to ${LOG_PATH}."
 echo "Installing conda."
 CONDA_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 if command -v curl &> /dev/null; then
-  curl -L -O "$CONDA_URL" > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+  curl -L --silent --show-error -O "$CONDA_URL" >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
 else
-  wget "$CONDA_URL" > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+  wget "$CONDA_URL" >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
 fi
-bash "Miniforge3-$(uname)-$(uname -m).sh" -b > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
-miniforge3/condabin/conda config --set auto_activate_base false > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
-miniforge3/condabin/conda init bash > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
-source $HOME/.bashrc > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+bash "Miniforge3-$(uname)-$(uname -m).sh" -b >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+eval "$(miniforge3/condabin/conda shell.bash hook)" >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+conda config --set auto_activate_base false >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+conda init bash >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
 
 echo "Creating the default Conda environment."
 DEFAULT_CONDA_ENV_NAME="default_env"
-conda create --name=${DEFAULT_CONDA_ENV_NAME} python > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
-conda activate ${DEFAULT_CONDA_ENV_NAME} > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+conda create -y --name=${DEFAULT_CONDA_ENV_NAME} python >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+conda activate ${DEFAULT_CONDA_ENV_NAME} >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
 
 echo "Installing tiko Python package."
-pip install tiko > ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+if [ -d "tiko" ] ; then
+    echo "Installing from local verison."
+    pip install -e tiko >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+else
+    pip install tiko >> ${LOG_PATH} 2> >(tee -a ${LOG_PATH} >&2)
+fi
 
 echo "Switching to the Python package portion of tiko."
 python -m tiko.install
